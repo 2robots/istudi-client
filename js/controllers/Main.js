409b026acfb6ba237ef726a88c314299d5083fc3
Franz Wilding <franzwilding@Franzs-MacBook-Pro.local>
2013-12-22 18:23:07 +0100

    add a dummy tutorial on startup

--- js/controllers/Main.js
+++ js/controllers/Main.js
@@ -1,7 +1,7 @@
 
 angular.module('istudi.MainCtrl', [])
 
-.controller('MainCtrl', function($scope, $rootScope, $models, $route, $download, $push, Platform) {
+.controller('MainCtrl', function($scope, $rootScope, $models, $route, $download, $push, Platform, Modal, $filter) {
 
 	$scope.toggleMenu = function() {
 		$scope.sideMenuController.toggleLeft();
@@ -45,4 +45,34 @@ angular.module('istudi.MainCtrl', [])
 		{ title: 'Einstellungen', reference: '#settings', icon: 'wrench' }, 
 		{ title: 'Über den VSStÖ', reference: '#about', icon: 'help-buoy' }
 	];
+	
+	
+	// Load the modal from the given template URL
+  Modal.fromTemplateUrl('helloModal.html', function(modal) {
+    $scope.modal = modal;
+    
+    // Open the hello modal, of there is no group selected
+    if($filter('filter')($rootScope.groups, {value: true}).length == 0) {
+      $scope.modal.show();
+    }   
+  }, {
+    // Use our scope for the scope of the modal to keep it simple
+    scope: $scope,
+    // The animation we want to use for the modal entrance
+    animation: 'slide-in-up'
+  });
+  
+  $scope.openModal = function() {
+    $scope.modal.show();
+  };
+  $scope.closeModal = function() {
+    $scope.modal.hide();
+    
+    // open menu
+    setTimeout(function(){
+      $scope.sideMenuController.toggleLeft();
+    }, 500);
+  };
+	
+	
 });
